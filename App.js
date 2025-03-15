6b3f4c9386136c54625ffd638bcd969b0fe9c76e
PeterTLoc <locnctse182454@fpt.edu.vn>
2025-03-15 16:37:55 +0700

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HELLO</Text>
+      <Text>Test thay đổi</Text>
       <StatusBar style="auto" />
     </View>
   );
